09f38e2d12bc8b8a4e32e3b29c2eefca72e834a9
romson <romerolijkwan@gmail.com>
2019-10-14 22:10:05 +0200

    library class

--- script.js
+++ script.js
@@ -1,47 +1,50 @@
-const myForm = document.querySelector('#myForm'),
-    bookTitle = document.querySelector('#title'),
-    athourName = document.querySelector('#author'),
-    pageCount = document.querySelector('#pages'),
-    imageSrc = document.querySelector('#image'),
-    myButton = document.querySelector('#myButton'),
-    container = document.querySelector('.container');
+const myForm = document.querySelector("#myForm"),
+	bookTitle = document.querySelector("#title"),
+	authorName = document.querySelector("#author"),
+	pageCount = document.querySelector("#pages"),
+	imageSrc = document.querySelector("#image"),
+	myButton = document.querySelector("#myButton"),
+	container = document.querySelector(".container");
 
-myButton.addEventListener('click', (e) => {
-  e.preventDefault();
-  addToLibrary();
+myForm.addEventListener("submit", e => {
+	e.preventDefault();
+	addToLibrary();
 });
 
 function addToLibrary() {
-  let book = `
+	let book = `
     <div class="zone col">
       <img src="${imageSrc.value}" width="100%" alt="book">
       <p>
         <strong>${bookTitle.value}</strong><br>
-        By: ${athourName.value}<br>
+        By: ${authorName.value}<br>
         Pagecount: ${pageCount.value}
       </p>
       <button onclick="deleteFromLibrary(this)">Remove</button>
-    </div>`
-  container.insertAdjacentHTML('afterbegin', book);
-  bookTitle.value = ''; athourName.value = ''; pageCount.value = '';
-  bookTitle.focus();
+    </div>`;
+	addToArray();
+	container.insertAdjacentHTML("afterbegin", book);
+	bookTitle.value = ""; authorName.value = ""; pageCount.value = "";
+	bookTitle.focus();
 }
 
 function deleteFromLibrary(itemToDelete) {
-  itemToDelete.parentElement.remove();
+	itemToDelete.parentElement.remove();
 }
 
 class Library {
-  constructor(title, author, pages, image) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.image = image;
-  }
+	constructor(title, author, pages, image) {
+		this.title = title; this.author = author;
+		this.pages = pages; this.image = image;
+	}
 }
 
 let book_array = [];
 
 function addToArray() {
-  // add book to array of objects
+	let bookName = `${bookTitle.value}`;
+	let author = `${authorName.value}`;
+	let pages = `${pageCount.value}`;
+	let img = `${imageSrc.value}`;
+	library.unshift(new Library(bookName, author, pages, img));
 }
