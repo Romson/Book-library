46bd57c856cbb5381900f301d5931ec6a364cc7a
romson <romerolijkwan@gmail.com>
2020-05-22 17:30:53 +0200

    port update

--- public/script.js
+++ public/script.js
@@ -1,11 +1,11 @@
 function bookInfo(book) {
-	return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+  return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
 	
 	<div class="mr-4">
 		<img src="${book.image}" width="100px">
 	</div>
   <div class="mr-auto mb-auto">
-    <div class="book-title">${book.title}</div>
+    <div class="book-title"><strong>${book.title}</strong></div>
     <div class="author-name">Author: ${book.author}</div>
     <div class="page-count">Pages: ${book.pages}</div>
   </div>
@@ -16,51 +16,51 @@ function bookInfo(book) {
 </li>`;
 }
 
-let ulHTML = bookObject.map(book => {
-	return bookInfo(book);
+let ulHTML = bookObject.map((book) => {
+  return bookInfo(book);
 });
 
 document.getElementById("book-list").insertAdjacentHTML("beforeend", ulHTML);
 
-document.addEventListener("click", e => {
-	// Edit feature
-	if (e.target.classList.contains("edit-me")) {
-		let newBookTitle = prompt(
-			"Edit book title:",
-			e.target.parentElement.parentElement.querySelector(".book-title")
-				.innerHTML
-		);
-		if (newBookTitle) {
-			axios
-				.post("/edit-title", {
-					title: newBookTitle,
-					id: e.target.getAttribute("data-id")
-				})
-				.then(() => {
-					e.target.parentElement.parentElement.querySelector(
-						".book-title"
-					).innerHTML = `<strong>${newBookTitle}</strong>`;
-				})
-				.catch(() => {
-					console.log("Try again later!");
-				});
-		}
-	}
+document.addEventListener("click", (e) => {
+  // Edit feature
+  if (e.target.classList.contains("edit-me")) {
+    let newBookTitle = prompt(
+      "Edit book title:",
+      e.target.parentElement.parentElement.querySelector(".book-title")
+        .innerHTML
+    );
+    if (newBookTitle) {
+      axios
+        .post("/edit-title", {
+          title: newBookTitle,
+          id: e.target.getAttribute("data-id"),
+        })
+        .then(() => {
+          e.target.parentElement.parentElement.querySelector(
+            ".book-title"
+          ).innerHTML = `<strong>${newBookTitle}</strong>`;
+        })
+        .catch(() => {
+          console.log("Try again later!");
+        });
+    }
+  }
 
-	// Delete feature
-	if (e.target.classList.contains("delete-me")) {
-		// alert(e.target.getAttribute("data-id"));
-		if (confirm("Delete item permanently?")) {
-			axios
-				.post("/delete-item", {
-					id: e.target.getAttribute("data-id")
-				})
-				.then(() => {
-					e.target.parentElement.parentElement.remove();
-				})
-				.catch(() => {
-					console.log("Try again later!");
-				});
-		}
-	}
+  // Delete feature
+  if (e.target.classList.contains("delete-me")) {
+    // alert(e.target.getAttribute("data-id"));
+    if (confirm("Delete item permanently?")) {
+      axios
+        .post("/delete-item", {
+          id: e.target.getAttribute("data-id"),
+        })
+        .then(() => {
+          e.target.parentElement.parentElement.remove();
+        })
+        .catch(() => {
+          console.log("Try again later!");
+        });
+    }
+  }
 });
